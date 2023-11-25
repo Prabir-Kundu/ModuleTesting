c22a288b96183ddbc25da5eec4dec269aaf40d56
Prabir Kundu <kundu.prabir2000@gmail.com>
2023-11-25 20:15:12 +0530

    version 1.0.3

--- toastymessages/build.gradle.kts
+++ toastymessages/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.prabirkundu.toastymessages"
-version = "1.0.2"
+version = "1.0.3"
 
 android {
     namespace = "com.prabirkundu.toastymessages"
@@ -63,7 +63,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.prabirkundu.toastymessages"
                 artifactId = "ToastyMassages"
-                version = "1.0.2"
+                version = "1.0.3"
             }
         }
     }
